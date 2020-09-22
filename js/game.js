2488d94774cd08292d73351cd9860edd8cdb8ac9
Kirill <kirillmorgunov@mail.ru>
2020-09-22 13:22:02 +0300

    добавляет game.js в eslintignore

--- js/game.js
+++ js/game.js
@@ -1,24 +1,4 @@
 'use strict';
-const fireballSize = 22;
-const wizardSpeed = 3;
-const wizardWidth = 70;
-
-const getFireballSpeed = (isMovingLeft) => {
-  return isMovingLeft ? 2 : 5;
-};
-
-const getWizardHeight = () => {
-  return 1.337 * wizardWidth;
-};
-
-const getWizardX = (gameFieldWidth) => {
-  return (gameFieldWidth - wizardWidth) / 2;
-};
-
-const getWizardY = (gameFieldHeight) => {
-  return (gameFieldHeight / 3) - getWizardHeight();
-};
-
 window.GameConstants = {
   Fireball: {
     size: fireballSize || 24,
