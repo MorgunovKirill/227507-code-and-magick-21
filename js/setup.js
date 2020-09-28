37348415be52036f8449014516a0e1b42e6f9629
Kirill <kirillmorgunov@mail.ru>
2020-09-28 22:23:20 +0300

    добавляет WIZARD_QUANTITY, getRandom, renderFragment

--- js/setup.js
+++ js/setup.js
@@ -4,9 +4,15 @@ const WIZARD_NAMES = [`Иван`, `Хуан Себастьян`, `Мария`, `
 const WIZARD_SURNAMES = [`да Марья`, `Верон`, `Мирабелла`, `Вальц`, `Онопко`, `Топольницкая`, `Нионго`, `Ирвинг`];
 const WIZARD_COATS = [`rgb (101, 137, 164)`, `rgb(241, 43, 107)`, `rgb(146, 100, 161)`, `rgb(56, 159, 117)`, `rgb(215, 210, 55)`, `rgb(0, 0, 0)`];
 const WIZARD_EYES = [`black`, `red`, `blue`, `yellow`, `green`];
+const WIZARD_QUANTITY = 4;
+
+const getRandomInteger = (min, max) => {
+  let rand = min + Math.random() * (max + 1 - min);
+  return Math.floor(rand);
+};
 
 const getRandomItem = (arr) => {
-  return arr[Math.floor(arr.length * Math.random())];
+  return arr[getRandomInteger(0, arr.length - 1)];
 };
 
 const generateWisard = (name, surname, coat, eyes) => {
@@ -27,13 +33,21 @@ const renderWizard = function (wizard) {
   return wizardElement;
 };
 
+const renderFragment = (arr, renderPlace) => {
+  let fragment = document.createDocumentFragment();
+
+  for (let i = 0; i < arr.length; i++) {
+    fragment.appendChild(renderWizard(arr[i]));
+  }
+
+  renderPlace.appendChild(fragment);
+};
+
 const wizards = [];
 
-wizards.push(generateWisard(WIZARD_NAMES, WIZARD_SURNAMES, WIZARD_COATS, WIZARD_EYES));
-wizards.push(generateWisard(WIZARD_NAMES, WIZARD_SURNAMES, WIZARD_COATS, WIZARD_EYES));
-wizards.push(generateWisard(WIZARD_NAMES, WIZARD_SURNAMES, WIZARD_COATS, WIZARD_EYES));
-wizards.push(generateWisard(WIZARD_NAMES, WIZARD_SURNAMES, WIZARD_COATS, WIZARD_EYES));
-
+for (let i = 0; i < WIZARD_QUANTITY; i++) {
+  wizards.push(generateWisard(WIZARD_NAMES, WIZARD_SURNAMES, WIZARD_COATS, WIZARD_EYES));
+}
 
 const setupWindow = document.querySelector(`.setup`);
 setupWindow.classList.remove(`hidden`);
@@ -43,12 +57,6 @@ const similarWizardTemplate = document.querySelector(`#similar-wizard-template`)
     .content
     .querySelector(`.setup-similar-item`);
 
-let fragment = document.createDocumentFragment();
-
-for (let i = 0; i < wizards.length; i++) {
-  fragment.appendChild(renderWizard(wizards[i]));
-}
-
-similarListElement.appendChild(fragment);
+renderFragment(wizards, similarListElement);
 
 document.querySelector(`.setup-similar`).classList.remove(`hidden`);
