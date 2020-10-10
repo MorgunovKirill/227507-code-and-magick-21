58f8cea7612bf719dd7c1eee78a968aa5f053a27
Kirill <kirillmorgunov@mail.ru>
2020-10-10 17:59:48 +0300

    добавляет перетаскивание модального окна

--- js/dialog.js
+++ js/dialog.js
@@ -1,9 +1,17 @@
 'use strict';
 
 (function () {
+  const setupDialogElement = window.util.setup;
   const setupOpen = document.querySelector(`.setup-open`);
-  const setupClose = window.util.setup.querySelector(`.setup-close`);
-  const setupName = window.util.setup.querySelector(`.setup-user-name`);
+  const setupClose = setupDialogElement.querySelector(`.setup-close`);
+  const setupName = setupDialogElement.querySelector(`.setup-user-name`);
+
+  const dialogHandle = setupDialogElement.querySelector(`.upload`);
+
+  const initialCoords = {
+    x: window.getComputedStyle(setupDialogElement).getPropertyValue(`left`),
+    y: window.getComputedStyle(setupDialogElement).getPropertyValue(`top`)
+  };
 
   const onPopupEscPress = function (evt) {
     if (document.activeElement !== setupName) {
@@ -15,15 +23,19 @@
     window.util.isEnterEvent(evt);
   };
 
+
   const openPopup = function () {
-    window.util.setup.classList.remove(`hidden`);
+    setupDialogElement.classList.remove(`hidden`);
+
+    setupDialogElement.style.left = initialCoords.x;
+    setupDialogElement.style.top = initialCoords.y;
 
     document.addEventListener(`keydown`, onPopupEscPress);
     setupName.addEventListener(`keydown`, onPopupEnterPress);
   };
 
   const closePopup = function () {
-    window.util.setup.classList.add(`hidden`);
+    setupDialogElement.classList.add(`hidden`);
 
     document.removeEventListener(`keydown`, onPopupEscPress);
     setupName.removeEventListener(`keydown`, onPopupEnterPress);
@@ -48,4 +60,53 @@
       closePopup();
     }
   });
+
+  dialogHandle.addEventListener(`mousedown`, function (evt) {
+    evt.preventDefault();
+
+    let startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    let dragged = false;
+
+    const onMouseMove = (moveEvt) => {
+      moveEvt.preventDefault();
+
+      dragged = true;
+
+      let shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      setupDialogElement.style.top = (setupDialogElement.offsetTop - shift.y) + `px`;
+      setupDialogElement.style.left = (setupDialogElement.offsetLeft - shift.x) + `px`;
+    };
+
+    const onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener(`mousemove`, onMouseMove);
+      document.removeEventListener(`mouseup`, onMouseUp);
+
+      if (dragged) {
+        const onClickPreventDefault = (clickEvt) => {
+          clickEvt.preventDefault();
+          dialogHandle.removeEventListener(`click`, onClickPreventDefault);
+        };
+        dialogHandle.addEventListener(`click`, onClickPreventDefault);
+      }
+    };
+
+    document.addEventListener(`mousemove`, onMouseMove);
+    document.addEventListener(`mouseup`, onMouseUp);
+  });
+
 })();
