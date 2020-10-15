dd424d20aeec875db124f683f8ce502390230935
Kirill <kirillmorgunov@mail.ru>
2020-10-15 12:05:35 +0300

    выносит повторяющийся код в statusHandler

--- js/backend.js
+++ js/backend.js
@@ -7,34 +7,7 @@
   };
   const TIMEOUT_IN_MS = 10000;
 
-  window.save = function (data, onLoad, onError) {
-    const xhr = new XMLHttpRequest();
-    xhr.responseType = `json`;
-
-    xhr.addEventListener(`load`, function () {
-      if (xhr.status === StatusCode.OK) {
-        onLoad(xhr.response);
-      } else {
-        onError(`Статус ответа: ` + xhr.status + ` ` + xhr.statusText);
-      };
-
-    xhr.addEventListener(`error`, function () {
-      onError(`Произошла ошибка соединения`);
-    });
-    xhr.addEventListener(`timeout`, function () {
-      onError(`Запрос не успел выполниться за ` + xhr.timeout + `мс`);
-    });
-
-    xhr.timeout = TIMEOUT_IN_MS;
-
-    xhr.open(`POST`, URL);
-    xhr.send(data);
-  };
-
-  window.load = function (onLoad, onError) {
-    const xhr = new XMLHttpRequest();
-    xhr.responseType = `json`;
-
+  const statusHandler = (xhr, onLoad, onError) => {
     xhr.addEventListener(`load`, function () {
       if (xhr.status === StatusCode.OK) {
         onLoad(xhr.response);
@@ -51,6 +24,23 @@
     });
 
     xhr.timeout = TIMEOUT_IN_MS;
+  };
+
+  window.save = function (data, onLoad, onError) {
+    const xhr = new XMLHttpRequest();
+    xhr.responseType = `json`;
+
+    statusHandler(xhr, onLoad, onError);
+
+    xhr.open(`POST`, URL);
+    xhr.send(data);
+  };
+
+  window.load = function (onLoad, onError) {
+    const xhr = new XMLHttpRequest();
+    xhr.responseType = `json`;
+
+    statusHandler(xhr, onLoad, onError);
 
     xhr.open(`GET`, URL_DATA);
     xhr.send();
